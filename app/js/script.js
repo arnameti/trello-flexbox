17dab7092e1ceb3d1adf21846391f2152f36cfa9
arnameti <arnameti@gmail.com>
2021-12-16 14:11:26 +0100

    trello-flexbox base settings

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+// // ********** nav toggle ************
+// // select button and links
+// const navBtn = document.getElementById("nav-toggle");
+// const links = document.getElementById("nav-list");
+// // add event listener
+// navBtn.addEventListener("click", () => {
+//   links.classList.toggle("show-links");
+// });
